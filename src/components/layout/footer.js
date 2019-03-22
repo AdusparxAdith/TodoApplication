1b9e665474e09bcc6af9d3a2c113f351e45f95e8
AdusparxAdith <adithdinesh97@gmail.com>
2019-03-22 17:48:12 +0530

    minor version bug fix

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer(){
     return(
         <div style={footerStyle}>
-            <p>~Made with love, by Adusparx</p>
+            <p>~Made with love, by Adusparx ~ v1.0</p>
         </div>
         
     )
