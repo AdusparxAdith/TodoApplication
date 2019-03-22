e8ac56dbd12b83f927ae84df0cbc96acfa7a0452
AdusparxAdith <adithdinesh97@gmail.com>
2019-03-22 13:33:52 +0530

    Updates

---
+++ precache-manifest.afd68edb91ac62e4d3c96c3ef117bdcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59bb54afedcf74a5241d",
+    "url": "/TodoApplication/static/js/runtime~main.cf8d2b92.js"
+  },
+  {
+    "revision": "676b375c2f602dcf25a9",
+    "url": "/TodoApplication/static/js/main.71eba2aa.chunk.js"
+  },
+  {
+    "revision": "15fd8828d396c49831b6",
+    "url": "/TodoApplication/static/js/2.98f68b41.chunk.js"
+  },
+  {
+    "revision": "676b375c2f602dcf25a9",
+    "url": "/TodoApplication/static/css/main.4efc0925.chunk.css"
+  },
+  {
+    "revision": "bde0705f8d1aca93454a9f9b2ad6cc8d",
+    "url": "/TodoApplication/index.html"
+  }
+];
